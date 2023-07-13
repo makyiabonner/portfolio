f642b322be2cd00a52671c403b424fcc7be77d8d
makyia bonner <makyiabonner@gmail.com>
2023-07-13 16:01:29 -0400

    feature:landing-page is complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -24,13 +21,20 @@ export default function Home() {
           </ul>
           <div className={styles.contactBtn}>
             <p>Contact</p>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
               <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
             </svg>
           </div>
         </div>
       </nav>
-      
+      <main className={styles.main}>
+        <section>
+          <h1 className={styles.landingTitle}>Hello, I'm Makyia Bonner</h1>
+          <h3 className={styles.landingSubtitle}>Front-end Developer</h3>
+        </section>
+      </main>
+      <svg  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path  d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill" fill="#001220" fill-opacity="1"></path></svg>
+      <section className={styles.secondMain}></section>
     </>
   )
 }
