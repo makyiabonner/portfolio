8566ce68632e3187f90c5ebee11ccfdfe72129e5
makyia bonner <makyiabonner@gmail.com>
2023-07-13 17:07:49 -0400

    chore: added more shape dividers

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,6 +35,15 @@ export default function Home() {
       </main>
       <svg  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path  d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill" fill="#001220" fill-opacity="1"></path></svg>
       <section className={styles.secondMain}></section>
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M1200 0L0 103.52 0 120 1200 120 1200 0z" className="shape-fill" fill="red"></path>
+      </svg>
+      <section className={styles.secondMain}></section>
+      <svg  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path  d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill" fill="#001220" fill-opacity="1"></path></svg>
+      <section className={styles.secondMain}></section>
+      <section className={styles.secondMain}></section>
+
+
     </>
   )
 }
