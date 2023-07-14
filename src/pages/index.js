968f8604f67241e01229bc786c3baabd3fa25aae
makyia bonner <makyiabonner@gmail.com>
2023-07-14 00:50:52 -0400

    feature: about section completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,25 +33,24 @@ export default function Home() {
           <h3 className={styles.landingSubtitle}>Front-end Developer</h3>
         </section>
       </main>
-      <svg  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" >
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 121" preserveAspectRatio="none" style={{background:"white",display:"block"}}>
         <path  d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className="shape-fill" fill="#012b4b" fill-opacity="1"></path>
       </svg>
       <section className={styles.secondMain}>
-        <div>
-          <h2>About Me</h2>
+        <div className={styles.aboutDiv}>
+          <h2 className={styles.subtitle}>About Me</h2>
+          <p className={styles.p}>
+            As a front-end engineer, I have a strong passion for coding, gaming, anime, and sports. 
+            My days are primarily spent programming, exploring new programming languages, and avidly following my favorite teams, the Braves, Falcons, and Hawks 
+            or indulging in my love for anime. I'm currently watching on the <i>"Hell's Paradise"</i> – definitely a good watch. 
+          </p>
         </div>
       </section>
-      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" style={{display: "block"}}>
-        <path d="M1200 0L0 103.52 0 120 1200 120 1200 0z" class="shape-fill" fill="#012b4b"></path>
-      </svg>
       <section className={styles.thirdMain}>
         <div>
           <h2>Projects</h2>
         </div>
       </section>
-      <svg  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path  d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill" fill="#012b4b" fill-opacity="1"></path>
-      </svg>
       <section className={styles.fourthMain}>
         <div>
           <h2>Contact</h2>
