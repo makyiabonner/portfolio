83b07b8ddaa9a4111b7e5b5e6925d4726898b9f5
makyia bonner <makyiabonner@gmail.com>
2023-07-14 18:11:58 -0400

    chore: tech component created

---
+++ src/components/TechStack/techStack.js
@@ -0,0 +1,11 @@
+import styles from '../TechStack/techStack.module.scss'
+
+export default function TechStack(){
+    return (
+        <section className={styles.thirdMain}>
+            <div className='m-auto'>
+                <h2>Tech Stack</h2>
+            </div>
+        </section>
+    )
+}
