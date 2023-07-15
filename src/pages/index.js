843feb750289d2f9bfcfb8be6451124a328bbb87
makyia bonner <makyiabonner@gmail.com>
2023-07-15 13:34:32 -0400

    feature: add projects cards for projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Projects from '@/components/Projects/projects';
 import TechStack from '@/components/TechStack/techStack';
 import Head from 'next/head'
 import NewNav from '../components/NewNav/newNav';
@@ -53,11 +54,7 @@ export default function Home() {
         </div>
       </section>
       <TechStack/>
-      <section className={styles.fourthMain}>
-        <div>
-          <h2>Contact</h2>
-        </div>
-      </section>
+      <Projects/>
       <div id='divider'>
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" style={{display: "block"}}>
           <path d="M1200 0L0 103.52 0 120 1200 120 1200 0z" class="shape-fill" fill="#012b4b"></path>
