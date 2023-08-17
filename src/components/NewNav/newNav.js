d7e2e00d7ba5ce1c87f39b688fa59d52d3990b65
makyia bonner <makyiabonner@gmail.com>
2023-08-17 15:45:02 -0400

    created nav and home screen

--- src/components/NewNav/newNav.js
+++ src/components/NewNav/newNav.js
@@ -1,57 +1,49 @@
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
+import Link from 'next/link';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import styles from '../NewNav/newNav.module.scss';
-import { useRef } from 'react';
 
 
-const NewNav = React.forwardRef((props, ref) => {
-    const homeRef = useRef(props.refHome);
-    const aboutRef = useRef(props.refAbout);
-    const projectRef = useRef(props.refWork);
-    const contactRef = useRef(props.refContact)
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+const NewNav = React.forwardRef((props) => {
 
     return (
         <>
-            <nav className={`${styles.nav} bg-primary`}>
-                <h1 className={styles.title}>MB</h1>
-                    <Button variant='primary' onClick={handleShow} className="d-sm-flex d-md-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                    </svg>
-                    </Button>
-
-                    <Offcanvas show={show} onHide={handleClose} className="w-50 bg-primary">
-                        <Offcanvas.Header className="variant-info" closeButton>
-                        <Offcanvas.Title>
-                            <h1 className={`${styles.title} text-info`}>MB</h1>
-                        </Offcanvas.Title>
-                        </Offcanvas.Header>
-                        <Offcanvas.Body>
-                        <ul className={`${styles.navList} d-block text-info`}>
-                            <li className={styles.navItem} onClick={() => props.homeRef.current.scrollIntoView({ behavior: 'smooth' })}>Home</li>
-                            <li className={styles.navItem} onClick={() => props.homeRef.current.scrollIntoView({ behavior: 'smooth' })}>About</li>
-                            <li className={styles.navItem} onClick={() => props.homeRef.current.scrollIntoView({ behavior: 'smooth' })}>Works</li>
-                        </ul>
-                        </Offcanvas.Body>
-                    </Offcanvas>
-                <div className={`${styles.div} d-none d-md-flex`}>
-                  <ul className={styles.navList}>
-                            <li className={styles.navItem} onClick={() => props.homeRef.current.scrollIntoView({ behavior: 'smooth' })}>Home</li>
-                            <li className={styles.navItem} onClick={() => props.homeRef.current.scrollIntoView({ behavior: 'smooth' })}>About</li>
-                            <li className={styles.navItem} onClick={() => props.homeRef.current.scrollIntoView({ behavior: 'smooth' })}>Works</li>
-                  </ul>
-                  <div className={styles.contactBtn}>
-                    <p className={styles.contactText}>Contact</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                      <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-                    </svg>
-                  </div>
-                </div>
+            <nav className={styles.nav}>
+                <ul className={styles.nav_list}>
+                    <li>
+                        <h5 className={styles.nav_list_item}>WORKS.</h5>
+                    </li>
+                    <li>
+                        <h5 className={styles.nav_list_item}>CONTACT.</h5>
+                    </li>
+                    <li>
+                        <svg width="56" height="42" viewBox="0 0 56 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <mask id="mask0_2_57" maskUnits="userSpaceOnUse" x="0" y="8" width="56" height="26">
+                                <path d="M2.9581 8.54545H11.5589L16.282 22.9631H16.5803L23.6896 8.54545H32.2905L30.1776 34H23.4162L24.6342 19.2841H24.4354L17.5746 33.8011H13.4979L9.0483 19.1847H8.84943L7.60653 34H0.84517L2.9581 8.54545ZM33.4526 34L35.5655 8.54545H46.6025C48.5414 8.54545 50.1489 8.80646 51.4249 9.32848C52.7092 9.8505 53.6497 10.5879 54.2463 11.5408C54.8429 12.4937 55.0873 13.6165 54.9796 14.9091C54.8967 15.8371 54.615 16.6864 54.1344 17.457C53.6621 18.2276 53.0365 18.8739 52.2576 19.396C51.4788 19.918 50.588 20.2784 49.5854 20.4773L49.5605 20.7259C50.6543 20.759 51.6279 21.0324 52.4814 21.5462C53.3348 22.0516 53.9894 22.7518 54.4451 23.6467C54.9009 24.5415 55.079 25.5897 54.9796 26.7912C54.8636 28.1832 54.3996 29.422 53.5875 30.5075C52.7755 31.5929 51.6735 32.4464 50.2814 33.0678C48.8977 33.6893 47.2819 34 45.4341 34H33.4526ZM40.8354 28.4815H44.0669C45.2187 28.4815 46.1053 28.262 46.7267 27.8228C47.3482 27.3836 47.6879 26.7415 47.7459 25.8963C47.7956 25.2997 47.7003 24.7943 47.4601 24.38C47.228 23.9657 46.8676 23.6508 46.3787 23.4354C45.8981 23.2199 45.3016 23.1122 44.589 23.1122H41.258L40.8354 28.4815ZM41.6309 18.8366H44.4647C45.0695 18.8366 45.6123 18.7414 46.0929 18.5508C46.5734 18.3602 46.9587 18.0826 47.2488 17.718C47.5388 17.3535 47.7045 16.9143 47.7459 16.4006C47.8122 15.6051 47.5761 15.0002 47.0375 14.5859C46.5072 14.1716 45.8153 13.9645 44.9618 13.9645H42.0286L41.6309 18.8366Z" fill="white"/>
+                            </mask>
+                            <g mask="url(#mask0_2_57)">
+                                <rect x="-4" y="27" width="60" height="4" fill={props.theme || 'white'}/>
+                                <rect x="-4" y="7" width="62" height="12" fill={props.theme || 'white'}/>
+                                <rect x="-4" y="21" width="60" height="3" fill={props.theme || 'white'}/>
+                                <rect x="-4" y="33" width="60" height="1" fill={props.theme || 'white'}/>
+                            </g>
+                        </svg>
+                    </li>
+                    <li>
+                        <h5 className={styles.nav_list_item}>SKILLS.</h5>
+                    </li>
+                    <li>
+                        <h5 className={styles.nav_list_item}>ABOUT ME.</h5>
+                    </li>
+                </ul>
+            <div className='d-none'>
+                <ul className={styles.list}>
+                  <Link className={styles.listItem} target='_blank' href='https://github.com/makyiabonner'><li className='m-1'><span className={`${styles.listItem} fs-6`}>Github</span></li></Link>
+                  <Link className={styles.listItem} target='_blank' href='https://www.linkedin.com/in/makyiabonner'><li className='m-1'><span className={`${styles.listItem} fs-6`}>LinkedIn</span></li></Link>
+                  <Link className={styles.listItem} target='_blank' href='https://docs.google.com/document/d/1aiLKdgDUF3-jUGbIXilmyvPXX4_fxPgmV409h_utTKg/edit?usp=sharing'><li className='m-1'><span className={`${styles.listItem} fs-6`}>CV Resume</span></li></Link>
+                </ul>
+                <span className={`${styles.copyright} fs-6 text-info mb-5`}>@2023 Makyia Bonner. All rights reserved.</span>
+            </div>
             </nav>
         </>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,10 @@
-import ContactForm from '@/components/Contact/contact';
-import Projects from '@/components/Projects/projects';
-import TechStack from '@/components/TechStack/techStack';
 import Head from 'next/head'
 import NewNav from '../components/NewNav/newNav';
 import styles from '../styles/Home.module.scss';
 import Link from 'next/link';
-import { useRef } from 'react';
 
 
 export default function Home() {
-  const homeRef = useRef(null)
-  const aboutRef = useRef(null)
-  const techRef = useRef(null);
-  const projectRef = useRef(null);
-  const contactRef = useRef(null);
-
-  const handleScrollClick = (ref) => {
-    ref.current.scrollIntoView({
-      behavior: 'smooth'
-    });
-  };
-  const currentYear = new Date().getFullYear();
   return (
     <>
       <Head>
@@ -29,82 +13,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NewNav refHome={homeRef} refAbout={aboutRef} refWorks={projectRef} refContact={contactRef}/>
-      <main ref={homeRef} className={styles.landingSection}>
-        <section>
-          <h1 className={styles.landingTitle}>Hello, I&apos;m Makyia Bonner</h1>
-          <h3 className={styles.landingSubtitle}>Front-end Developer</h3>
-          <div className='m-auto p-auto'>
-            <Link className='pe-2' target='_blank' href='https://www.linkedin.com/in/makyiabonner/' id='LinkedIn'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-                <path className='text-secondary' d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
-              </svg>
-            </Link>
-            <Link className='pe-2' target='_blank' href='https://github.com/makyiabonner' id='Github'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-                <path className='text-secondary' d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-              </svg>
-            </Link>
-            <Link className='pe-2' target='_blank' href='https://docs.google.com/document/d/1aiLKdgDUF3-jUGbIXilmyvPXX4_fxPgmV409h_utTKg/edit?usp=sharing' id='Resume'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-envelope-paper-fill" viewBox="0 0 16 16">
-                <path className='text-secondary' fill-rule="evenodd" d="M6.5 9.5 3 7.5v-6A1.5 1.5 0 0 1 4.5 0h7A1.5 1.5 0 0 1 13 1.5v6l-3.5 2L8 8.75l-1.5.75ZM1.059 3.635 2 3.133v3.753L0 5.713V5.4a2 2 0 0 1 1.059-1.765ZM16 5.713l-2 1.173V3.133l.941.502A2 2 0 0 1 16 5.4v.313Zm0 1.16-5.693 3.337L16 13.372v-6.5Zm-8 3.199 7.941 4.412A2 2 0 0 1 14 16H2a2 2 0 0 1-1.941-1.516L8 10.072Zm-8 3.3 5.693-3.162L0 6.873v6.5Z"/>
-              </svg>
-            </Link>
-          </div>
-        </section>
-      </main>
-      <div id='divider'>
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 121" preserveAspectRatio="none" style={{background:"white"}}>
-          <path  d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className="shape-fill" fill="#012b4b" fill-opacity="1"></path>
-        </svg>
-      </div>
-      <section ref={aboutRef} className={styles.secondMain}>
-        <div className={styles.aboutDiv}>
-          <h2 className={styles.subtitle}>About Me</h2>
-          <p className={`${styles.p} p-xl-5`}>
-            As a front-end engineer, I have a strong passion for coding, gaming, anime, and sports. 
-            My days are primarily spent programming, exploring new programming languages, and avidly following my favorite teams, the Braves, Falcons, and Hawks 
-            or indulging in my love for anime. I&apos;m currently watching on the <i>&quot;Hell&apos;s Paradise&quot;</i> – definitely a good watch. 
-          </p>
+      <section className={styles.home}>
+        <div className={styles.hero}>
+          <h3 className={styles.small_title}>FRONT-END DEVELOPER</h3>
+          <h1 className={styles.big_title}>HELLO<br></br>IM MAKYIA
+            <h1 className={styles.big_title} style={{color:'#FF0000', display:'inline'}}>.</h1>
+          </h1>
+        </div>
+        <div className={styles.nav}>
+          <NewNav theme='#FF0000'/>
         </div>
       </section>
-      <div ref={techRef}>
-        <TechStack/>
-      </div>
-      <div ref={projectRef}>
-        <Projects/>
-      </div>
-      <div ref={contactRef}>
-        <ContactForm/> 
-      </div>
-      <div id='divider'>
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" style={{display: "block", background:'ghostwhite'}}>
-          <path d="M1200 0L0 103.52 0 120 1200 120 1200 0z" class="shape-fill" fill="#012b4b"></path>
-        </svg>
-      </div>
-      <footer className={`${styles.footer} p-1`}>
-        <section className='d-flex w-100 p-sm-3 p-md-0 justify-content-evenly'>
-          <div>
-            <h6 className={`${styles.listTitle} fs-5 text-info`}>Navigation</h6>
-            <ul className={styles.list}>
-              <li className='m-1'><span className={`${styles.listItem} btn p-0 fs-6`} onClick={() => handleScrollClick(homeRef)}>Home</span></li>
-              <li className='m-1'><span className={`${styles.listItem} btn p-0 fs-6`} onClick={() => handleScrollClick(aboutRef)}>About Me</span></li>
-              <li className='m-1'><span className={`${styles.listItem} btn p-0 fs-6`} onClick={() => handleScrollClick(techRef)}>Tech Stack</span></li>
-              <li className='m-1'><span className={`${styles.listItem} btn p-0 fs-6`} onClick={() => handleScrollClick(projectRef)}>Projects</span></li>
-              <li className='m-1'><span className={`${styles.listItem} btn p-0 fs-6`} onClick={() => handleScrollClick(contactRef)}>Contact</span></li>
-            </ul>
-          </div>
-          <div>
-            <h6 className={`${styles.listTitle} fs-5 text-info`}>External Accounts</h6>
-            <ul className={styles.list}>
-              <Link className={styles.listItem} target='_blank' href='https://github.com/makyiabonner'><li className='m-1'><span className={`${styles.listItem} fs-6`}>Github</span></li></Link>
-              <Link className={styles.listItem} target='_blank' href='https://www.linkedin.com/in/makyiabonner'><li className='m-1'><span className={`${styles.listItem} fs-6`}>LinkedIn</span></li></Link>
-              <Link className={styles.listItem} target='_blank' href='https://docs.google.com/document/d/1aiLKdgDUF3-jUGbIXilmyvPXX4_fxPgmV409h_utTKg/edit?usp=sharing'><li className='m-1'><span className={`${styles.listItem} fs-6`}>CV Resume</span></li></Link>
-            </ul>
-          </div>
-        </section>
-        <span className={`${styles.copyright} fs-6 text-info mb-5`}>@{currentYear} Makyia Bonner. All rights reserved.</span>
-      </footer>
     </>
   )
 }
