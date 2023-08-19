1b523255f3368b71f80f19be542a41e32c2654e0
makyia bonner <makyiabonner@gmail.com>
2023-08-19 11:48:30 -0400

    created general setup for skills page

---
+++ src/pages/skills.js
@@ -0,0 +1,18 @@
+import styles from '../styles/Skills.module.scss';
+
+export default function Skills(){
+    return(
+        <>
+         <container className={styles.page}>
+            <section className={styles.left_side}>
+                <div className={styles.left_content}>
+                    
+                </div>
+            </section>
+            <section className={styles.right_side}>
+
+            </section>
+         </container>
+        </>
+    )
+}
