de11c651a858a5bc810bf66886e9b8bf798bbab1
makyia bonner <makyiabonner@gmail.com>
2023-09-13 17:45:30 -0400

    refactor minor bugs

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,7 +15,7 @@ export default function Contact() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className={styles.page}>
-      <video  className={styles.video} autoPlay muted loop playsInLine>
+      <video className={styles.video} autoPlay muted loop playsInline>
          <source src="./videos/contact.mp4" type="video/mp4" />
       </video>
         <main className={`${styles.main} `}>
